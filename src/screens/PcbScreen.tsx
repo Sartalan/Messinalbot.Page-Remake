a5a43236e5d91252440d775a96955d55e961a03e
Sarti <elpinguinoalan@gmail.com>
2024-07-08 01:41:31 -0300

    PCB-Screen: List Improvement | v0.8

--- src/screens/PcbScreen.tsx
+++ src/screens/PcbScreen.tsx
@@ -1,9 +1,70 @@
 export function PcbScreen() {
+
+    interface Item {
+        id: number;
+        text: string;
+        url: string;
+
+    }
+
+    const data: Item[] = [
+        {
+            "id": 1,
+            "text": "Microcontrolador: ESP32",
+            "url": "#"
+        },
+        {
+            "id": 2,
+            "text": "Puente H: DRV8833",
+            "url": "#"
+        },
+        {
+            "id": 3,
+            "text": "S. Ultrasonido: HC-SR05 (2)",
+            "url": "#"
+        },
+        {
+            "id": 4,
+            "text": "S. Infrarrojo : CNY70 (2)",
+            "url": "#"
+        },
+        {
+            "id": 5,
+            "text": "Regulador de Voltaje (1)",
+            "url": "#"
+        },
+        {
+            "id": 6,
+            "text": "Motores Amarillos (2)",
+            "url": "#"
+        },
+        {
+            "id": 7,
+            "text": "Borneras (8)",
+            "url": "#"
+        },
+        {
+            "id": 8,
+            "text": "Resistencias (4)",
+            "url": "#"
+        },
+        {
+            "id": 9,
+            "text": "Transistores (2)",
+            "url": "#"
+        },
+        {
+            "id": 10,
+            "text": "Capacitores (4)",
+            "url": "#"
+        }
+    ]
+
     return (
-        <article id="PCB" className="bg-black max-h-screen p-14 flex flex-col items-center w-full h-full">
+        <article id="PCB" className="bg-black h-screen pt-14 flex flex-col items-center w-full ">
             <div className="flex flex-col p-6 gap-4">
                 <h3 className="title-page text-5xl select-none">PCB - ROBOT</h3>
-                <p className=" bg-page text-page text-pretty p-4 text-3xl">Circuito con sensores infrarrojos y de ultrasonido, botón de encendido, y la capacidad de controlar dos motores. A más detalles de su funcionamiento leer la documentación respectiva en el repositorio de Github del proyecto.</p>
+                <p className=" bg-page text-page text-pretty p-4 text-3xl select-none">Circuito con sensores infrarrojos y de ultrasonido, botón de encendido, y la capacidad de controlar dos motores. A más detalles de su funcionamiento leer la documentación respectiva en el repositorio de Github del proyecto.</p>
             </div>
 
             <div className=" flex gap-10 p-6 ">
@@ -12,14 +73,14 @@ export function PcbScreen() {
 
                 </div>
                 <div className="  w-full flex-grow-0 p-5">
-                    <h4 className="text-center italic p-3 text-white text-5xl select-none ">Componentes:</h4>
+                    <h4 className="text-start italic p-3 text-white text-5xl select-none ">Componentes:</h4>
                     <hr className="list-bottom-line-style m-3" />
-                    <ul className=" flex flex-col gap-2 max-h-40  overflow-y-scroll">
-                        <li className="hover:bg-red-700 rounded w-11/12 p-1" ><a className="text-list-style whitespace-nowrap italic font-thin  text-2xl block " href="#">Microcontrolador: ESP32</a></li>
-                        <li className="hover:bg-red-700 w-11/12 p-1" ><a className="text-list-style whitespace-nowrap italic font-thin  text-2xl block " href="#">Microcontrolador: ESP32</a></li>
-                        <li className="hover:bg-red-700 w-11/12 p-1" ><a className="text-list-style whitespace-nowrap italic font-thin  text-2xl block " href="#">Microcontrolador: ESP32</a></li>
-                        <li className="hover:bg-red-700 w-11/12 p-1" ><a className="text-list-style whitespace-nowrap italic font-thin  text-2xl block " href="#">Microcontrolador: ESP32</a></li>
-                       
+                    <ul className="flex flex-col gap-2 max-h-40 scrollbar scrollbar-thumb-gray-700 scrollbar-track-black overflow-y-scroll">
+                        {
+                            data.map((data: Item) => (
+                                <li key={data.id} className=" hover:bg-cyan-900 rounded w-11/12 " ><a className="text-list-style whitespace-nowrap h-full p-1 m-1 italic font-thin text-2xl block" href={data.url}>{data.text}</a></li>
+                            ))
+                        }
                     </ul>
                 </div>
             </div>
